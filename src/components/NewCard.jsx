d8f14cad124b81b9912f67a4d063adbd7878dca2
AlexWalkerGD <alexwalkerson@hotmail.com>
2025-12-31 00:44:59 +0000

    create new note

---
+++ src/components/NewCard.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import api from "../services/GetApi";
+import { useRef } from "react";
+
+const NewCard = ({ closeCard }) => {
+  const inputTitle = useRef();
+  const inputContent = useRef();
+
+  async function createNote() {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      alert("Unauthorized");
+      return;
+    }
+    try {
+      await api.post(
+        "/notes",
+        {
+          title: inputTitle.current.value,
+          content: inputContent.current.value,
+        },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      closeCard();
+    } catch (error) {
+      console.error({ error });
+    }
+  }
+
+  return (
+    <div className=" flex flex-col bg-cyan-900 w-50 h-50 pt-5 px-3 pb-2 rounded-xl ">
+      <input
+        className="text-white font-bold text-xl text-center w-45 outline-none "
+        type="text"
+        placeholder="Title"
+        ref={inputTitle}
+      />
+      <input
+        className="text-gray-300 pt-2 w-45 outline-none pb-20"
+        type="text"
+        placeholder="Content..."
+        ref={inputContent}
+      />
+      <div className=" flex justify-center ">
+        <button
+          className=" rounded-md bg-gray-300 h-7 w-12 border-none font-bold cursor-pointer hover:bg-gray-400"
+          type="button"
+          onClick={createNote}
+        >
+          Ok
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NewCard;
--- src/routes/Notes.jsx
+++ src/routes/Notes.jsx
@@ -1,14 +1,24 @@
 import React from "react";
-import axios from "axios";
 import api from "../services/GetApi";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import NoteCard from "../components/NoteCard.jsx";
+import NewCard from "../components/NewCard.jsx";
 
 const Notes = () => {
   const [notes, setNotes] = useState([]);
+  const [card, setCard] = useState(false);
+
   const navigate = useNavigate();
+
+  async function getNotes(token) {
+    const notesResponse = await api.get("/notes", {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    setNotes(notesResponse.data);
+  }
 
   useEffect(() => {
     const validateUser = async () => {
@@ -27,25 +37,35 @@ const Notes = () => {
 
         console.log(`Authorized`);
 
-        const notesResponse = await api.get("/notes", {
-          headers: { Authorization: `Bearer ${token}` },
-        });
-
-        console.log("Notes:", notesResponse.data);
-        setNotes(notesResponse.data);
+        getNotes(token);
       } catch (error) {
         console.log(error);
       }
     };
     validateUser();
-  }, []);
+  }, [notes]);
 
   return (
     <div>
-      <h1 className="text-3xl text-center font-semibold pt-5 text-cyan-800">
-        Your Notes
-      </h1>
-      <div className="flex flex-col gap-5 items-center pt-10">
+      <div className="flex flex-row justify-between pt-10 ">
+        <span className="px-10"></span>
+        <h1 className="text-3xl text-center flex items-center font-semibold text-cyan-800">
+          Your Notes
+        </h1>
+        <button
+          className="bg-cyan-900 w-10 h-10 m-2 text-3xl text-white text-center pb-2 cursor-pointer hover:bg-cyan-700"
+          onClick={() => setCard(true)}
+        >
+          +
+        </button>
+      </div>
+
+      {card && (
+        <div className="flex flex-col gap-5 items-center pt-10">
+          <NewCard closeCard={() => setCard(false)} />
+        </div>
+      )}
+      <div className="grid grid-cols-2 gap-5 items-center pt-10 px-7">
         {notes.map((note) => (
           <div>
             <NoteCard title={note.title} description={note.content} />
